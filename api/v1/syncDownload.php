7e3a8538e8e6b608453fd107c73aa6fe00c97ccb
Sean Flo <sean@cushmantech.net>
2017-11-18 20:45:17 -0800

    Valid JSON now.

--- api/v1/syncDownload.php
+++ api/v1/syncDownload.php
@@ -19,14 +19,13 @@ curl_close($ch1);
 $teamsAtEvents = array();
 foreach (json_decode($districtEventsJson) as $event) {
 
-    $url2 = $baseUrl.'event/'.$event['key'].'/events/keys';
+    $url2 = $baseUrl.'event/'.$event->key.'/teams/simple';
     $ch2 = curl_init($url2);
     curl_setopt($ch2, CURLOPT_HTTPHEADER, array('X-TBA-Auth-Key: '.$TBAAuthKey));
     curl_setopt($ch2, CURLOPT_RETURNTRANSFER, true);
-    $teamsAtEvents[$event['key']] = curl_exec($ch2);
+    $teamsAtEvents[$event->key] = curl_exec($ch2);
     curl_close($ch2);
-    echo json_encode($teamsAtEvents);
 }
+echo json_encode($teamsAtEvents);
 ?>
 }
-<?php var_dump($teamsAtEvents); ?>
