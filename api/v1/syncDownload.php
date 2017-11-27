c018d518feef57a80af8cd962b4959455b4000b9
MoSadie <sean@cushmantech.net>
2017-11-27 11:29:01 -0800

    Nvm, forgot ";"

--- api/v1/syncDownload.php
+++ api/v1/syncDownload.php
@@ -19,11 +19,12 @@ curl_close($ch1);
  "TeamsByEvent" : [
 	 <?php #Request 2: Teams for each event
 foreach (json_decode($districtEventsJson) as $event) {
+	
     $url2 = $baseUrl.'event/'.$event->key.'/teams/simple';
     $ch2 = curl_init($url2);
     curl_setopt($ch2, CURLOPT_HTTPHEADER, array('X-TBA-Auth-Key: '.$TBAAuthKey));
     curl_setopt($ch2, CURLOPT_RETURNTRANSFER, true);
-	echo "{ \"EventKey\": \"".$event->key."\", ".curl_exec($ch2)."},".PHP_EOL
+	echo "{ \"EventKey\": \"".$event->key."\", ".curl_exec($ch2)."},".PHP_EOL;
     curl_close($ch2);
 }
 ?>
