cf0e911c9b09e5a3434ece7b5d826ce0eed12f51
MoSadie <sean@cushmantech.net>
2017-12-18 14:07:52 -0800

    Biggerer buttons on homepage

--- index.php
+++ index.php
@@ -78,7 +78,7 @@ function loadTeamAtEvent(team,event) {
 		echo "<p>Team ".$teamNumber." has been scouted at these event codes:</p>";
 		foreach($events as $event) {
 			$eventCode = explode("/",$event)[2];
-			echo "<p><button style='font-size: larger;' onClick='window.location.href=\"viewTeam.php?eventCode=".$eventCode."&teamNumber=".$teamNumber."\"'>".$eventCode."</button></p>";
+			echo "<p><button style='font-size: 30;' onClick='window.location.href=\"viewTeam.php?eventCode=".$eventCode."&teamNumber=".$teamNumber."\"'>".$eventCode."</button></p>";
 		}
 		
 		echo "<br/><p><button onClick='window.location.href=\"index.php\"'>Go Back</button>";
