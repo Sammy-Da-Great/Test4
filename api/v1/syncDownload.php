3a2b3488233151f5520a652cf67d12d2f686c5d5
MoSadie <sean@cushmantech.net>
2018-01-13 13:01:22 -0800

    Let's try something different.

--- api/v1/syncDownload.php
+++ api/v1/syncDownload.php
@@ -176,7 +176,7 @@ foreach ($data["Events"] as $event) {
 		$dataToWrite = $result2["header"]["last-modified"]."\n".str_replace("\n","",$result2["body"]);
 		file_put_contents($teamAtEventCacheDir.$event["key"].".json", $dataToWrite);
 	}
-	$data["TeamsByEvent"] = $data["TeamsByEvent"] + array($tmpData);
+	$data["TeamsByEvent"] = array_merge($data["TeamsByEvent"],array($tmpData));
 	var_dump($data["TeamsByEvent"]);
 	unset($tmpData);
 }
