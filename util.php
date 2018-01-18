4af828e23f87c7b9877a6ab4938cd811a370538a
Sean Flo <sean@cushmantech.net>
2018-01-17 20:55:28 -0800

    Clever girl...

--- util.php
+++ util.php
@@ -2,7 +2,7 @@
 function logToFile($stringToLog) {
 	$logFile = fopen("debug.log","a");
 	$date = getDate();
-	fwrite($logFile, "[".$date["hours"].":"$date["minutes"].":".$date["seconds"]." ".$date["month"]." ".$date["mday"].", ".$date["year"]."]: ".$stringToLog."\n");
+	fwrite($logFile, "[".$date["hours"].":".$date["minutes"].":".$date["seconds"]." ".$date["month"]." ".$date["mday"].", ".$date["year"]."]: ".$stringToLog."\n");
 	fclose($logFile);
 }
 ?>
