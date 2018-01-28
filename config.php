bbf2886510d2a6980d58b8b02187ece3231dd0ff
MoSadie <sean@cushmantech.net>
2018-01-28 15:57:51 -0800

    Remove my access key from the config
    And invalidated it too.

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
-$configLoaded = true; #This just allows the script to check if the config is loaded.
+$configLoaded = true; #This just allows the scripts to check if the config is loaded.
 $districtKey = "pnw"; #This is the district key for your district minus the year.
-$TBAAuthKey = "3kqlXvrEcFIrkkFXIODyaHmDLeidPaeTiky2s7P1Wlh3SMptNN36HqnOAzcFA5KA";#"SetMeToYourAuthKey"; #You can get an auth key from https://www.thebluealliance.com/account
-$seasonYear = 2018; #For getting data from TBA
+$TBAAuthKey = "SetMeToYourAuthKey"; #You can get an auth key from https://www.thebluealliance.com/account
+$seasonYear = 2018; #Current year of the season, as of kickoff. (Ex FIRST POWER UP = 2018)
 $hiddenDataKey = "noalliance" #Type this key into the input box to toggle hidden data.
 ?>
