b629ec8db492290d4082cfe1b103c2111505ee8f
Sean Flo <sean@cushmantech.net>
2018-02-16 22:30:12 -0800

    Created branch for export tool. Begins work on #5.

---
+++ exportData.php
@@ -0,0 +1,32 @@
+<head>
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+<link rel="icon" href="favicon.ico" type="image/x-icon">
+<title>Export Data - ORF Scouter</title>
+<style>
+body {
+	background-color: black;
+}
+img {
+	width: 25%;
+}
+p {
+	font-size: 25;
+}
+p, h1, h2, h3 {
+	color: white;
+}
+
+div {
+    position: absolute;
+    top:0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+
+    margin: auto;
+	text-align: center;
+}
+</style></head>
+<body>
+<div>
+<h1>Export Data - ORF Scouter</h1>
